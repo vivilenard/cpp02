4387440e66bf4435b85ffdbf2ad55ff5c3337e60
VIVI <vivienlenard00@gmail.com>
2023-05-23 19:11:38 +0200

    looks fresh

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -1,6 +1,7 @@
 #include "Fixed.hpp"
 
 Fixed::Fixed()	{std::cout << "Default constructor called"	<< std::endl;}
+Fixed::~Fixed()	{std::cout << "Destructor called" 			<< std::endl;}
 
 Fixed::Fixed(const int	int_number)
 {
@@ -13,39 +14,29 @@ Fixed::Fixed(const float f)
 {
 	std::cout << "Float constructor called" << std::endl;
 
-	float buf = f * (1 << _n_factional_bits);
-	int		i = round(buf);
-	this->setRawBits(i);
+	this->setRawBits(f * (1 << _n_factional_bits));
 }
-
-Fixed::~Fixed()	{std::cout << "Destructor called" 			<< std::endl;}
-
 
 Fixed::Fixed(const Fixed & copy)
 {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = copy;
-	//this->_number_value = copy.getRawBits();
 }
 
-int		Fixed::getRawBits( void ) const 
-{
+int		Fixed::getRawBits( void ) const 	{
 	return this->_number_value;
 }
 
-void	Fixed::setRawBits( int const raw )
-{
+void	Fixed::setRawBits( int const raw )	{
 	this->_number_value = raw;
 }
 
-float Fixed::toFloat( void ) const  //fxed to float
-{
-	return ((float)getRawBits() / (1 << this->_n_factional_bits)); //--> 2^fractional_bits  --> 2^8
-}
+float	Fixed::toFloat( void ) const	{
+	return ((float)getRawBits() / (1 << this->_n_factional_bits));
+} 										//--> 2^fractional_bits  --> 2^8
 
-int		Fixed::toInt( void ) const //fixed to int
-{
-	return ((int)round(toFloat()));
+int		Fixed::toInt( void ) const	{
+	return ((int)roundf(toFloat()));
 }
 
 Fixed &	Fixed::operator=(Fixed const & rhs)
