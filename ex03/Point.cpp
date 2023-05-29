39ed8b58851cbc17931b36210e63e52f283886db
VIVI <vivienlenard00@gmail.com>
2023-05-29 16:44:03 +0200

    eval ready

--- ex03/Point.cpp
+++ ex03/Point.cpp
@@ -10,22 +10,11 @@ Point::Point(Point const & a):_x(a._x), _y(a._y){}
 
 Point::~Point(){}
 
+Fixed	Point::getX() const{return this->_x;}
+Fixed	Point::getY() const{return this->_y;}
+
 Point & Point::operator=(Point const & p)
 {
 	(void)p;
 	return *this;
 }
-
-Fixed	Point::getX() const{return this->_x;}
-Fixed	Point::getY() const{return this->_y;}
-// Point Point::operator+(Point const &rhs) const
-// {
-// 	Point constresult;
-
-// 	result._x = this->_x + rhs._x;
-// 	return (result);
-// }
-
-// Point Point::operator-(Point const &rhs) const
-
-// Point Point::operator*(Point const &rhs) const
